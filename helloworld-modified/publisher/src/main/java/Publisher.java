1a8d198fed3f940f2199433b6815028a7d661e28
D4RKOPS <102692790+D4RKOPS@users.noreply.github.com>
2024-06-10 01:44:16 -0500

    Observer

--- helloworld-modified/publisher/src/main/java/Publisher.java
+++ helloworld-modified/publisher/src/main/java/Publisher.java
@@ -1,30 +1,21 @@
-//
-// Copyright (c) ZeroC, Inc. All rights reserved.
-//
+// Publisher.java
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import Demo.ClockPrx;
+import Demo.DataRequestPrx;
 import com.zeroc.Ice.CommunicatorDestroyedException;
 
-public class Publisher
-{
-    public static void main(String[] args)
-    {
+public class Publisher {
+    private static String currentData;
+
+    public static void main(String[] args) {
         int status = 0;
         java.util.List<String> extraArgs = new java.util.ArrayList<String>();
 
-        //
-        // Try with resources block - communicator is automatically destroyed
-        // at the end of this try block
-        //
-        try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args, "config.pub", extraArgs))
-        {
+        try (com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args, "config.pub", extraArgs)) {
             communicator.getProperties().setProperty("Ice.Default.Package", "com.zeroc.demos.IceStorm.clock");
-            //
-            // Install shutdown hook to (also) destroy communicator during JVM shutdown.
-            // This ensures the communicator gets destroyed when the user interrupts the application with Ctrl-C.
-            //
+
             Runtime.getRuntime().addShutdownHook(new Thread(() -> communicator.destroy()));
 
             status = run(communicator, extraArgs.toArray(new String[extraArgs.size()]));
@@ -32,60 +23,39 @@ public class Publisher
         System.exit(status);
     }
 
-    public static void usage()
-    {
+    public static void usage() {
         System.out.println("Usage: [--datagram|--twoway|--oneway] [topic]");
     }
 
-    private static int run(com.zeroc.Ice.Communicator communicator, String[] args)
-    {
+    private static int run(com.zeroc.Ice.Communicator communicator, String[] args) {
         String topicName = "time";
 
         com.zeroc.IceStorm.TopicManagerPrx manager = com.zeroc.IceStorm.TopicManagerPrx.checkedCast(
                 communicator.propertyToProxy("TopicManager.Proxy"));
-        if(manager == null)
-        {
+        if (manager == null) {
             System.err.println("invalid proxy");
             return 1;
         }
 
-        //
-        // Retrieve the topic.
-        //
         com.zeroc.IceStorm.TopicPrx topic;
-        try
-        {
+        try {
             topic = manager.retrieve(topicName);
-        }
-        catch(com.zeroc.IceStorm.NoSuchTopic e)
-        {
-            try
-            {
+        } catch (com.zeroc.IceStorm.NoSuchTopic e) {
+            try {
                 topic = manager.create(topicName);
-            }
-            catch(com.zeroc.IceStorm.TopicExists ex)
-            {
+            } catch (com.zeroc.IceStorm.TopicExists ex) {
                 System.err.println("temporary failure, try again.");
                 return 1;
             }
         }
 
-        //
-        // Get the topic's publisher object, and create a Clock proxy with
-        // the mode specified as an argument of this application.
-        //
         com.zeroc.Ice.ObjectPrx publisher = topic.getPublisher();
-
-        String message="";
 
         ClockPrx clock = ClockPrx.uncheckedCast(publisher);
         BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("publishing tick events. Press ^C to terminate the application.");
-        try
-        {
-            // String client = System.getProperty("user.name") + ":" + java.net.InetAddress.getLocalHost().getHostName() + "=";
-
+        try {
             System.out.print("Enter a function to integrate ('exit' to quit): \n");
             String userInput = consoleInput.readLine();
             if (!userInput.equals("exit")) {
@@ -94,35 +64,27 @@ public class Publisher
 
                 System.out.print("Enter upper limit: \n");
                 double upperLimit = Double.parseDouble(consoleInput.readLine());
-                message =  userInput;
+                currentData = userInput + "=" + lowerLimit + "," + upperLimit;
 
-
-                //service.printString(message,lowerLimit, upperLimit, clprx);
+                clock.tick("New integration task available");
             } else {
                 System.out.println("Bye bye!");
             }
 
-            while(true)
-            {
-
-                clock.tick(message);
-                /* clock.tick(date.format(new java.util.Date()));
-
-                try
-                {
+            while (true) {
+                try {
                     Thread.currentThread();
                     Thread.sleep(1000);
+                } catch (java.lang.InterruptedException e) {
                 }
-                catch(java.lang.InterruptedException e)
-                {
-                }*/
             }
-        }
-        catch(CommunicatorDestroyedException | IOException ex)
-        {
-            // Ctrl-C triggered shutdown hook, which destroyed communicator - we're terminating
+        } catch (CommunicatorDestroyedException | IOException ex) {
         }
 
         return 0;
+    }
+
+    public String getData() {
+        return currentData;
     }
 }
--- helloworld-modified/subscriber/src/main/java/Subscriber.java
+++ helloworld-modified/subscriber/src/main/java/Subscriber.java
@@ -1,91 +1,71 @@
-//
-// Copyright (c) ZeroC, Inc. All rights reserved.
-//
-
+// Subscriber.java
 import java.util.UUID;
-
 import com.zeroc.Ice.Util;
-
 import Demo.Clock;
+import Demo.DataRequestPrx;
+import Demo.PublisherPrx;
 
-public class Subscriber
-{
-    public static class ClockI implements Clock
-    {
+public class Subscriber {
+    public static class ClockI implements Clock {
+        private PublisherPrx publisher;
+
+        public ClockI(PublisherPrx publisher) {
+            this.publisher = publisher;
+        }
+
         @Override
-        public void tick(String date, com.zeroc.Ice.Current current)
-        {
+        public void tick(String message, com.zeroc.Ice.Current current) {
+            System.out.println(message);
 
-            System.out.println(date);
+            if (message.equals("New integration task available")) {
+                String data = publisher.getData();
+                System.out.println("Received data: " + data);
+            }
         }
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int status = 0;
         java.util.List<String> extraArgs = new java.util.ArrayList<String>();
 
         com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args, "config.sub", extraArgs);
-        //
-        // Destroy communicator during JVM shutdown
-        //
         Thread destroyHook = new Thread(() -> communicator.destroy());
         Runtime.getRuntime().addShutdownHook(destroyHook);
 
-        try
-        {
+        try {
             status = run(communicator, destroyHook, extraArgs.toArray(new String[extraArgs.size()]));
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
             ex.printStackTrace();
             status = 1;
         }
 
-        if(status != 0)
-        {
+        if (status != 0) {
             System.exit(status);
         }
-        //
-        // Else the application waits for Ctrl-C to destroy the communicator
-        //
     }
 
-    public static void usage()
-    {
+    public static void usage() {
         System.out.println("Usage: [--batch] [--datagram|--twoway|--ordered|--oneway] " +
                 "[--retryCount count] [--id id] [topic]");
     }
 
-    private static int run(com.zeroc.Ice.Communicator communicator, Thread destroyHook, String[] args)
-    {
-
+    private static int run(com.zeroc.Ice.Communicator communicator, Thread destroyHook, String[] args) {
         String topicName = "time";
 
         com.zeroc.IceStorm.TopicManagerPrx manager = com.zeroc.IceStorm.TopicManagerPrx.checkedCast(
                 communicator.propertyToProxy("TopicManager.Proxy"));
-        if(manager == null)
-        {
+        if (manager == null) {
             System.err.println("invalid proxy");
             return 1;
         }
 
-        //
-        // Retrieve the topic.
-        //
         com.zeroc.IceStorm.TopicPrx topic;
-        try
-        {
+        try {
             topic = manager.retrieve(topicName);
-        }
-        catch(com.zeroc.IceStorm.NoSuchTopic e)
-        {
-            try
-            {
+        } catch (com.zeroc.IceStorm.NoSuchTopic e) {
+            try {
                 topic = manager.create(topicName);
-            }
-            catch(com.zeroc.IceStorm.TopicExists ex)
-            {
+            } catch (com.zeroc.IceStorm.TopicExists ex) {
                 System.err.println("temporary failure, try again.");
                 return 1;
             }
@@ -93,62 +73,35 @@ public class Subscriber
 
         com.zeroc.Ice.ObjectAdapter adapter = communicator.createObjectAdapter("Clock.Subscriber");
 
-        //
-        // Add a servant for the Ice object. If --id is used the
-        // identity comes from the command line, otherwise a UUID is
-        // used.
-        //
-        // id is not directly altered since it is used below to detect
-        // whether subscribeAndGetPublisher can raise
-        // AlreadySubscribed.
-        //
+        PublisherPrx publisher = PublisherPrx.checkedCast(communicator.stringToProxy("SimplePublisher:default -p 10000"));
 
-        com.zeroc.Ice.ObjectPrx subscriber = adapter.add(new ClockI(), Util.stringToIdentity(UUID.randomUUID().toString()));
-
-        //
-        // Activate the object adapter before subscribing.
-        //
+        com.zeroc.Ice.ObjectPrx subscriber = adapter.add(new ClockI(publisher), Util.stringToIdentity(UUID.randomUUID().toString()));
         adapter.activate();
 
         java.util.Map<String, String> qos = new java.util.HashMap<>();
 
-
-        try
-        {
+        try {
             topic.subscribeAndGetPublisher(qos, subscriber);
-        }
-        catch(com.zeroc.IceStorm.AlreadySubscribed e)
-        {
+        } catch (com.zeroc.IceStorm.AlreadySubscribed e) {
             System.out.println("reactivating persistent subscriber");
-        }
-        catch(com.zeroc.IceStorm.InvalidSubscriber e)
-        {
+        } catch (com.zeroc.IceStorm.InvalidSubscriber e) {
             e.printStackTrace();
             return 1;
-        }
-        catch(com.zeroc.IceStorm.BadQoS e)
-        {
+        } catch (com.zeroc.IceStorm.BadQoS e) {
             e.printStackTrace();
             return 1;
         }
 
-        //
-        // Replace the shutdown hook to unsubscribe during JVM shutdown
-        //
         final com.zeroc.IceStorm.TopicPrx topicF = topic;
         final com.zeroc.Ice.ObjectPrx subscriberF = subscriber;
-        Runtime.getRuntime().addShutdownHook(new Thread(() ->
-        {
-            try
-            {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
                 topicF.unsubscribe(subscriberF);
-            }
-            finally
-            {
+            } finally {
                 communicator.destroy();
             }
         }));
-        Runtime.getRuntime().removeShutdownHook(destroyHook); // remove old destroy-only shutdown hook
+        Runtime.getRuntime().removeShutdownHook(destroyHook);
 
         return 0;
     }
